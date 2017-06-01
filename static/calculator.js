a674fbf24c876d8324053a5fcca3e4ddbdc6bcc1
Pavlos Ratis <dastergon@dastergon.gr>
2017-06-01 15:52:20 +0300

    refactoring js

--- static/calculator.js
+++ static/calculator.js
@@ -30,49 +30,46 @@ function downtimeToMs(val) {
 };
 
 function availabilityCalculator() {
+
     var percentage = document.getElementById("availability");
     var currentLevel = percentage.value;
-	var output;
-	if (isNaN(currentLevel) || currentLevel < 1 || currentLevel > 100) {
-			output = "<em>Input not valid. Please input a number between 1 and 100.</em>";
-	} else {
-		output =
-			"<table id='custom-table'>"
-			+ "<tr>"
-			+ "<th>Availability level</th>"
-			+ "<th>Downtime per year</th>"
-			+ "<th>Downtime per quarter</th>"
-			+ "<th>Downtime per month</th>"
-			+ "<th>Downtime per week</th>"
-			+ "<th>Downtime per day</th>"
-			+ "<th>Downtime per hour</th>"
-			+ "</tr>";
+    var output;
 
-			var ms_per_hour = downtimeToMs(currentLevel);
-			var ms_per_day = ms_per_hour * 24;
-			var ms_per_week = ms_per_day * 7;
-			var ms_per_year = ms_per_day * 365.2425; // Gregorian, on average (https://en.wikipedia.org/wiki/Year#Summary)
-			var ms_per_month = ms_per_year / 12;
-			var ms_per_quarter = ms_per_year / 4;
+    if (isNaN(currentLevel) || currentLevel < 1 || currentLevel > 100) {
+        output = "<em>Input not valid. Please input a number between 1 and 100.</em>";
+    } else {
+        output =
+            "<table id='custom-table'>"
+            + "<tr>"
+            + "<th>Availability level</th>"
+            + "<th>Downtime per year</th>"
+            + "<th>Downtime per quarter</th>"
+            + "<th>Downtime per month</th>"
+            + "<th>Downtime per week</th>"
+            + "<th>Downtime per day</th>"
+            + "<th>Downtime per hour</th>"
+            + "</tr>";
 
-			var downtime_per_year    =  msToDowntime(ms_per_year);
-			var downtime_per_quarter =  msToDowntime(ms_per_quarter);
-			var downtime_per_month =  msToDowntime(ms_per_month);
-			var downtime_per_week =  msToDowntime(ms_per_week);
-			var downtime_per_day =  msToDowntime(ms_per_day);
-			var downtime_per_hour =  msToDowntime(ms_per_hour);
+            var ms_per_hour = downtimeToMs(currentLevel)
+                , ms_per_day = ms_per_hour * 24
+                , ms_per_week = ms_per_day * 7
+                , ms_per_year = ms_per_day * 365.2425 // Gregorian, on average (https://en.wikipedia.org/wiki/Year#Summary)
+                , ms_per_month = ms_per_year / 12
+                , ms_per_quarter = ms_per_year / 4;
 
-			output += "<tr>"
-				+ "<td>" + currentLevel + "%</td>"
-				+ "<td>" + showTime(downtime_per_year) + "</td>"
-				+ "<td>" + showTime(downtime_per_quarter) + "</td>"
-				+ "<td>" + showTime(downtime_per_month) + "</td>"
-				+ "<td>" + showTime(downtime_per_week) + "</td>"
-				+ "<td>" + showTime(downtime_per_day) + "</td>"
-				+ "<td>" + showTime(downtime_per_hour) + "</td>"
-				+ "</tr>";
+            var categories = [ms_per_year, ms_per_quarter
+                , ms_per_month, ms_per_week, ms_per_day
+                , ms_per_hour];
 
-		output += "</table>";
-	}
+            output += "<tr><td>" + currentLevel + "%</td>";
+            for (i = 0; i < categories.length; i++) {
+                if (currentLevel == 100) {
+                    output += "<td>" + 0 + "</td>";
+                } else {
+                    output += "<td>" + showTime(msToDowntime(categories[i])) + "</td>";
+                }
+            }
+            output += "</tr></table>";
+    }
     document.getElementById("availability-table").innerHTML = output;
 };
