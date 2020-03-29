59470a3ff4a5adabb434ec12384440d7cac7d5ac
Pavlos Ratis <dastergon@gmail.com>
2020-03-29 01:44:20 +0100

    UI refresh

--- static/calculator.js
+++ static/calculator.js
@@ -36,18 +36,18 @@ function availabilityCalculator() {
     var output;
 
     if (isNaN(currentLevel) || currentLevel <= 0 || currentLevel > 100) {
-        output = "<em>Input not valid. Please input a number between 0 and 100.</em>";
+        output = "<em class=\"tc\">Input not valid. Please input a number between 0 and 100.</em>";
     } else {
         output =
-            "<table id='custom-table'>"
+            "<table class=\"f6 w-100 mw8 center\" cellspacing=\"0\">"
             + "<tr>"
-            + "<th>Availability level</th>"
-            + "<th>Downtime per year</th>"
-            + "<th>Downtime per quarter</th>"
-            + "<th>Downtime per month</th>"
-            + "<th>Downtime per week</th>"
-            + "<th>Downtime per day</th>"
-            + "<th>Downtime per hour</th>"
+            + "<th class=\"fw6 bb b--black-20 pb3 \">Availability level</th>"
+            + "<th class=\"fw6 bb b--black-20 pb3 pr3 \">Downtime per year</th>"
+            + "<th class=\"fw6 bb b--black-20 pb3 pr3 \">Downtime per quarter</th>"
+            + "<th class=\"fw6 bb b--black-20 pb3 pr3 \">Downtime per month</th>"
+            + "<th class=\"fw6 bb b--black-20 pb3 pr3 \">Downtime per week</th>"
+            + "<th class=\"fw6 bb b--black-20 pb3 pr3 \">Downtime per day</th>"
+            + "<th class=\"fw6 bb b--black-20 pb3 pr3 \">Downtime per hour</th>"
             + "</tr>";
 
             var ms_per_hour = downtimeToMs(currentLevel)
@@ -61,12 +61,12 @@ function availabilityCalculator() {
                 , ms_per_month, ms_per_week, ms_per_day
                 , ms_per_hour];
 
-            output += "<tr><td>" + currentLevel + "%</td>";
+        output += "<tr><td class=\"pr3 bb b--black-20\">" + currentLevel + "%</td>";
             for (i = 0; i < categories.length; i++) {
                 if (currentLevel == 100) {
-                    output += "<td>" + 0 + "</td>";
+                    output += "<td class=\"pr3 bb b--black-20\">" + 0 + "</td>";
                 } else {
-                    output += "<td>" + showTime(msToDowntime(categories[i])) + "</td>";
+                    output += "<td class=\"pr3 bb b--black-20\">" + showTime(msToDowntime(categories[i])) + "</td>";
                 }
             }
             output += "</tr></table>";
